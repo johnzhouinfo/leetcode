068a4cdeefbd9a8f39be2b1199f6c489446854b4
Lingjue Zhou <lingjue.zhou@vivachek.com>
2022-08-09 14:20:21 +0800

    feat(*): add Q2.Add Two Numbers

---
+++ src/main/java/binaryTreesWithFactors/Main.java
@@ -0,0 +1,9 @@
+package binaryTreesWithFactors;
+
+/**
+ * @author: Lingjue
+ * @date: 2022/8/9
+ * @Description:
+ */
+public class Main {
+}
---
+++ src/main/java/binaryTreesWithFactors/Solution.java
@@ -0,0 +1,12 @@
+package binaryTreesWithFactors;
+
+/**
+ * @author: Lingjue
+ * @date: 2022/8/9
+ * @Description:
+ */
+public class Solution {
+    public int numFactoredBinaryTrees(int[] arr) {
+        return 1;
+    }
+}
